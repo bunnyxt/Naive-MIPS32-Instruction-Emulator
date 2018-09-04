8e370d98c16ded3b5082564d20dc527cc6dbc344
bunnyxt <bunnyxt@outlook.com>
2018-09-04 10:10:16 +0800

    Finish Memory class.

--- NaiveMIPS32InstructionEmulator/NaiveMIPS32InstructionEmulator/Memory.cpp
+++ NaiveMIPS32InstructionEmulator/NaiveMIPS32InstructionEmulator/Memory.cpp
@@ -1,5 +1,68 @@
 #include "NaiveMIPS32InstructionEmulator.h"
 
 Memory::Memory() {
-	
+	//initialize memory data
+	for (int i = 0; i < 4096; i++)//
+	{
+		data[i] = 0;
+	}
+}
+
+word Memory::ReadWord(address add)
+{
+	word result = 0;
+
+	//remodify address
+	add %= MEMSIZE;
+
+	//align address
+	add -= add % 4;
+
+	//assemble result word
+	for (int i = 3; i >= 0; i--)
+	{
+		result += data[add + i];
+		if (i != 0)
+		{
+			result <<= 8;
+		}
+	}
+
+	return result;
+}
+
+byte Memory::ReadByte(address add)
+{
+	byte result = 0;
+
+	//remodify address
+	add %= MEMSIZE;
+
+	result = data[add];
+
+	return result;
+}
+
+void Memory::WriteWord(address add, word w)
+{
+	//remodify address
+	add %= MEMSIZE;
+
+	//align address
+	add -= add % 4;
+
+	//write word
+	for (int i = 0; i < 4; i++)
+	{
+		data[add + i] = (w & ((0x000000FF) << (i * 8))) >> (i * 8);
+	}
+}
+
+void Memory::WriteByte(address add, byte b)
+{
+	//remodify address
+	add %= MEMSIZE;
+
+	//write byte
+	data[add] = b;
 }
--- NaiveMIPS32InstructionEmulator/NaiveMIPS32InstructionEmulator/NaiveMIPS32InstructionEmulator.h
+++ NaiveMIPS32InstructionEmulator/NaiveMIPS32InstructionEmulator/NaiveMIPS32InstructionEmulator.h
@@ -6,6 +6,15 @@
 
 using namespace std;
 
+//type definition
+typedef unsigned int instruction;
+typedef unsigned int address;
+typedef unsigned int word;
+typedef unsigned char byte;
+
+//const declaration
+const int MEMSIZE = 4096;//4096 = 4KB
+
 //class declaration
 class IOHelper;
 class Memory;
@@ -23,6 +32,13 @@ public:
 class Memory {
 public:
 	Memory();
+	word ReadWord(address add);
+	byte ReadByte(address add);
+	void WriteWord(address add, word w);
+	void WriteByte(address add, byte b);
+
+private:
+	byte data[MEMSIZE];
 };
 
 //Register class
