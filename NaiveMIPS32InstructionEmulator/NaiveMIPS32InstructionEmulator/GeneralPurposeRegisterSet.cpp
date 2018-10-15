cc935436612fd0c5f2c63f3bb840bff676222bc2
bunnyxt <bunnyxt@outlook.com>
2018-10-16 01:05:04 +0800

    Fix Set function to avoid accessing to not existed register with invalid index.

--- NaiveMIPS32InstructionEmulator/NaiveMIPS32InstructionEmulator/GeneralPurposeRegisterSet.cpp
+++ NaiveMIPS32InstructionEmulator/NaiveMIPS32InstructionEmulator/GeneralPurposeRegisterSet.cpp
@@ -31,10 +31,15 @@ void GeneralPurposeRegisterSet::Set(int index, word w)
 		cout << "[GeneralPurposeRegisterSet] Warning! Cannot change value of register r0 since r0 remains 0 constantly." << endl;
 		IOHelper::WriteLog("[GeneralPurposeRegisterSet] Warning! Cannot change value of register r0 since r0 remains 0 constantly.");
 	}
-	else
+	else if (index > 0 && index < 32)
 	{
 		cout << "[GeneralPurposeRegisterSet] Set register r" << index << " with value " << ConvertHelper::SeperateString(ConvertHelper::WordToString(w)) << "(" << w << ")." << endl;
 		IOHelper::WriteLog("[GeneralPurposeRegisterSet] Set register r" + to_string(index) + " with value " + ConvertHelper::SeperateString(ConvertHelper::WordToString(w)) + "(" + to_string(w) + ").");
 		gpr[index].Set(w);
+	}
+	else
+	{
+		cout << "[GeneralPurposeRegisterSet] Warning! Cannot find register r" << index << " in gprs!" << endl;
+		IOHelper::WriteLog("[GeneralPurposeRegisterSet] Warning! Cannot find register r" + to_string(index) + " in gprs!");
 	}
 }
